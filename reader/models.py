513547009a40495b7d7d0541d088a44fb0f3946b
Ankit Goyal <goyal.ankit.049@gmail.com>
2016-03-29 00:15:56 +0530

    path changes

--- reader/models.py
+++ reader/models.py
@@ -1,37 +1,41 @@
 from django.db import models
 from django.conf import settings
+import os
 
 def get_file_path(instance, filename):
 	ext = filename.split('.')[-1]
 	filename = "book%s.%s" % (instance.isbn, ext)
-	profilePath = (os.path.join(settings.BASE_DIR,'media'+os.sep+'books'+os.sep+str(folder_day)))
+	profilePath = (os.path.join(settings.BASE_DIR,'media'+os.sep+'books'+os.sep))
 	return os.path.join(profilePath,filename)
 
 def get_file_path_coverimages(instance, filename):
 	ext = filename.split('.')[-1]
 	filename = "bookcoverimage%s.%s" % (instance.isbn, ext)
-	coverimage = (os.path.join(settings.BASE_DIR,'media'+os.sep+'coverimage'+os.sep+str(folder_day)))
+	coverimage = (os.path.join(settings.BASE_DIR,'media'+os.sep+'coverimage'+os.sep))
 	return os.path.join(coverimage,filename)
 
 class Book(models.Model):
-    author = models.CharField(max_length=255)
-    isbn = models.CharField(max_length=255)
-    bookName = models.CharField(max_length=255)
-    bookEpub = models.FileField(upload_to=get_file_path,blank=True,null=True)
-    coverImageUrl = models.ImageField(upload_to='/media/coverimage',blank=True,null=True)
-    pub_date = models.DateTimeField('Date Published')
+	author = models.CharField(max_length=255)
+	isbn = models.CharField(max_length=255)
+	bookName = models.CharField(max_length=255)
+	bookEpub = models.FileField(upload_to=get_file_path,blank=True,null=True)
+	coverImageUrl = models.ImageField(upload_to=get_file_path_coverimages,blank=True,null=True)
+	pub_date = models.DateTimeField('Date Published')
 
-    def __str__(self):
-        return self.bookName
+	def __str__(self):
+		return self.bookName
 
-    def has_image(self):
-        image_list = []
-        if self.featured_image:
-            image_list.append("/media/coverimages/"+self.get_file_name())
-        return image_list
+	def has_image(self):
+		image_list = []
+		if self.featured_image:
+			image_list.append("/media/coverimage/"+self.get_file_name())
+		return image_list
 
-    def get_file_name(self):
-        return self.get_folder_day()+"/"+self.featured_image.path.split(os.sep)[-1]
+	def get_file_name(self):
+		return self.coverImageUrl.path.split(os.sep)[-1]
+
+	def get_cover_image_path(self):
+		return "/media/coverimage/"+self.get_file_name()
 
 class BookMark(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
