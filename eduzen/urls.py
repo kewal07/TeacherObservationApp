406ed135a500debb25a31841a8194f9f53317c9b
Ubuntu <ubuntu@ip-172-31-18-116.ap-southeast-1.compute.internal>
2016-04-03 20:13:27 +0000

    server changes

--- eduzen/urls.py
+++ eduzen/urls.py
@@ -10,10 +10,11 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^', include('reader.urls',namespace="reader")),
     url(r'^admin/', admin.site.urls),
-	url(r'^user/',include('login.urls',namespace="login")),
+    url(r'^user/',include('login.urls',namespace="login")),
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^accounts/', include('allauth.urls')),
+    url(r'^accounts/', include('allauth.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+admin.site.site_header = settings.ADMIN_SITE_HEADER
--- eduzen/wsgi.py
+++ eduzen/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ['HTTPS'] = "on"
+sys.path.append("/home/ubuntu/EReader_Django")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eduzen.settings")
 
 application = get_wsgi_application()
