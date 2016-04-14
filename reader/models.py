9c7c7c1ced871cab5faf8bd90ec995342f642690
Ankit Goyal <goyal.ankit.049@gmail.com>
2016-04-14 10:39:32 +0530

    commit GET to post and login not required

--- reader/models.py
+++ reader/models.py
@@ -46,7 +46,7 @@ class BookMark(models.Model):
     pub_date = models.DateTimeField(blank=True, null=True, auto_now_add=True)
 
     def __str__(self):
-        return self.book + "_" + self.user + "_" + self.bookmarkName
+        return self.book.bookName + "_" + self.user + "_" + self.bookmarkName
 
 class Highlight(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
