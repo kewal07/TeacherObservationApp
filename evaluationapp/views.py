2946c14dca8552fd413393736b477324409d66d6
Ankit Goyal <goyal.ankit.049@gmail.com>
2016-07-30 14:54:28 +0530

    add state parameter in redirect uri

--- evaluationapp/views.py
+++ evaluationapp/views.py
@@ -1034,7 +1034,8 @@ def get_token_from_code(auth_code, redirect_uri):
         	'redirect_uri': redirect_uri,
         	'scope': ' '.join(str(i) for i in scopes),
         	'client_id': 'ac92782f-c87a-48e5-9b4d-9ba250c8b11b',
-        	'client_secret': 'FJb8apcLnH98+bpg8uPWbqOOur7qF4LZ8oyQDoSZhk4='
+        	'client_secret': 'FJb8apcLnH98+bpg8uPWbqOOur7qF4LZ8oyQDoSZhk4=',
+        	'state': '12345'
     	}
 	r = requests.post(token_url, data = post_data)
 	try:
